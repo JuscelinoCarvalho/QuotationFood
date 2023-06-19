3a2a35697db242c1f027fe4448766663e880fd51
JuscelinoCarvalho <juscelino.carvalho@gmail.com>
2023-06-19 02:31:18 -0300

    Second Commit

--- src/main/java/jussa/quotation/food/model/QuotationFoodDTO.java
+++ src/main/java/jussa/quotation/food/model/QuotationFoodDTO.java
@@ -1,4 +1,33 @@
 package jussa.quotation.food.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class QuotationFoodDTO {
+
+    private Integer id_boletim;
+    private String date;
+    private String guerraProductAtClassification;
+    private String group;
+    private String product;
+    private String variety;
+    private String classificação;
+    private double minor_price;
+    private double common_price;
+    private double major_price;
+    private String unity;
+    private double weight;
+    private double tolerance;
+
+
 }
+/*
+*
+* Boletim	Data	Guerra Produto + Classificação	Guerra	Grupo	Produto	Variedade	Classificação	Menor	Comum	Maior	Unidade	Peso	Tolerância
+213	16/06/2023	1@193	1	FRUTAS	ABACATE	AVOCADO/HASS/FUERTE	A	3,9	4,95	6,23	KG	1	0
+
+*
+* */
